18c8c247c63c51d4969e8a8e0e94ffe92b84b37f
The3211 <aiyaz.parmar@gmail.com>
2022-07-19 21:16:46 +0530

    adapter in MovieActivity.kt

--- app/src/main/java/com/aiyaz/tmbdclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/aiyaz/tmbdclient/presentation/movie/MovieActivity.kt
@@ -3,10 +3,13 @@ package com.aiyaz.tmbdclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aiyaz.tmbdclient.R
+import com.aiyaz.tmbdclient.data.model.movie.Movie
 import com.aiyaz.tmbdclient.databinding.ActivityMovieBinding
 import com.aiyaz.tmbdclient.presentation.di.Injector
 import javax.inject.Inject
@@ -20,14 +23,34 @@ class MovieActivity : AppCompatActivity() {
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
 
+    private lateinit var adapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
         (application as Injector).createMovieSubComponent().inject(this)
         movieViewModel = ViewModelProvider(this, factory)[MovieViewModel::class.java]
+        initRecyclerView()
+    }
 
+
+    private fun initRecyclerView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularFunction()
+    }
+
+    private fun displayPopularFunction(){
+        binding.movieProgressBar.visibility = View.VISIBLE
         movieViewModel.getMovies().observe(this) {
-            Log.i("MyTag",it.toString())
+            if(it != null){
+                adapter.setMovieList(it)
+                adapter.notifyItemRangeInserted(0,it.size)
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+            }
         }
     }
 }
---
+++ app/src/main/java/com/aiyaz/tmbdclient/presentation/movie/MovieAdapter.kt
@@ -0,0 +1,45 @@
+package com.aiyaz.tmbdclient.presentation.movie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.aiyaz.tmbdclient.R
+import com.aiyaz.tmbdclient.data.model.movie.Movie
+import com.aiyaz.tmbdclient.databinding.ListItemBinding
+import com.bumptech.glide.Glide
+
+class MovieAdapter: RecyclerView.Adapter<MyViewHolder>() {
+
+    private val movieList = ArrayList<Movie>()
+
+    fun setMovieList(movies : List<Movie>){
+        movieList.clear()
+        movieList.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding: ListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.list_item,parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(movieList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+}
+
+class MyViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+    fun bind(movie: Movie){
+        binding.titleTextView.text = movie.title
+        binding.descriptionTextView.text = movie.overview
+        val posterUrl = "https://image.tmdb.org/t/p/w500" + movie.posterPath
+        Glide.with(binding.imageView.context)
+            .load(posterUrl)
+            .into(binding.imageView)
+    }
+}
